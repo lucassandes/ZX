0813ee8cf2454f96ca323c205f2bf0bfe50591af
lucassandes <lucassandes@msn.com>
2017-12-11 08:05:18 -0200

    Refactor getPoc function

--- www/components/products/products.controller.js
+++ www/components/products/products.controller.js
@@ -60,40 +60,87 @@
         };
 
         function queryProducts(categoryId, search) {
+            vm.errorMessage="";
+            if(pocId){
+                categoryId = (typeof categoryId !== 'undefined') ? categoryId : 0;
+                search = (typeof search !== 'undefined')  ? search : "";
+                vm.products = [];
 
-            categoryId = (typeof categoryId !== 'undefined') ? categoryId : 0;
-            search = (typeof search !== 'undefined')  ? search : "";
-            vm.products = [];
+                vm.loading = true;
+                vm.emptyMessage = "";
+                vm.errorMessage = "";
+                console.log("Query Products...");
+                console.log("pocId:", pocId);
+                console.log("categoryId:", categoryId);
+                console.log("search:", search);
 
-            vm.loading = true;
-            vm.emptyMessage = "";
-            vm.errorMessage = "";
-            console.log("Query Products...");
-            console.log("pocId:", pocId);
-            console.log("categoryId:", categoryId);
-            console.log("search:", search);
+                productsService
+                    .getProducts(pocId, categoryId, search)
+                    .then(function successCallback(response) {
+                        console.log(response.data); 
+                        var products = response.data.data.poc.products;
+                        products.forEach(element => {
+                            element.productVariants[0].quantity = "";
+                            vm.products.push(element.productVariants[0]);
+                        });
 
-            productsService
-                .getProducts(pocId, categoryId, search)
-                .then(function successCallback(response) {
-                    console.log(response.data); 
-                    var products = response.data.data.poc.products;
-                    products.forEach(element => {
-                        element.productVariants[0].quantity = "";
-                        vm.products.push(element.productVariants[0]);
+                        console.log("lenght:", products.length);
+                        if(products.length == 0){
+                            vm.emptyMessage = "Sorry, bro. We couldn't find anything";
+                        }
+                        vm.loading = false;
+                    }, function errorCallback(response) {
+                        vm.loading = false;
+                        vm.errorMessage = "Ops... Error :(";
+                        console.log("Ops... Error :(");
                     });
-
-                    console.log("lenght:", products.length);
-                    if(products.length == 0){
-                        vm.emptyMessage = "Sorry, bro. We couldn't find anything";
-                    }
-                    vm.loading = false;
-                }, function errorCallback(response) {
-                    vm.loading = false;
-                    vm.errorMessage = "Ops... Error :(";
-                    console.log("Ops... Error :(");
-                });
+            }
+            else {
+                vm.errorMessage = "Sorry, no POCs found for this adress";
+            }
         }
+
+
+       var getLastPoc = function (pocSearch){
+            //returns the last POC from pocSearch array that delivers in up to 1h
+            var id = false;
+            pocSearch.forEach(element => {
+                console.log(element);
+                var i = 0;
+                for (i = 0; i < element.deliveryTypes.length; i++){
+                    var typeId = element.deliveryTypes[i].deliveryTypeId;
+                    console.log ("pocId", element.id);
+                    console.log ("TypeId", typeId);
+                    if(typeId == 166) {
+                        id = element.id;
+                        return id;
+                        
+                    }
+                }
+            });
+
+            return id;
+        };
+
+
+        
+
+        var getFirstPoc = function (pocSearch){
+            //returns the first POC from pocSearch array that delivers in up to 1h
+            var j = 0; 
+            var id = false;
+            for (j = 0; j < pocSearch.length; j++) {
+                var element = pocSearch[j];
+                var i = 0;
+                for (i = 0; i < element.deliveryTypes.length; i++){
+                    var typeId = element.deliveryTypes[i].deliveryTypeId;
+                    if(typeId == 166) {
+                        id = element.id;
+                        return id;
+                    }
+                }
+            }
+        };
 
         var getPoc = function () {
             vm.errorMessage = "";
@@ -101,16 +148,18 @@
             productsService
                 .getPoc(geolocation)
                 .then(function successCallback(response) {
-                    console.log(response.data);
-                    var pocs = response.data;
-                    //console.log(pocs); console.log(pocs.data.pocSearch); ID 243;
-                    pocs
-                        .data
-                        .pocSearch
-                        .forEach(element => {
-                            console.log(element);
-                        });
-                    pocId = pocs.data.pocSearch[0].id;
+                    //console.log(response.data);
+                    var pocSearch = response.data.data.pocSearch;
+
+                    //If we use this routine below, POC will be 242
+                    pocId = getLastPoc(pocSearch);
+
+                    //If we use this routine below, POC will be 243
+                    pocId = getFirstPoc(pocSearch);
+               
+
+                    //get the first POC means this below??
+                    //pocId = pocs.data.pocSearch[0].id;
                     queryProducts();
 
                 }, function errorCallback(response) {
